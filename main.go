9f037acc4e14508efa726b974c140b27632a21a6
islishude <lishude@aliyun.com>
2019-09-07 22:28:10 +0800

    chore: v2.0.0

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "v1")
+		fmt.Fprintf(w, "v2")
 	})
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
