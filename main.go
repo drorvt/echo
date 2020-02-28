0299d50ee4c1dcbac6abaa53fa088116a48e20c8
islishude <islishude@gmail.com>
2020-02-28 22:19:57 +0800

    chore: add file log

--- main.go
+++ main.go
@@ -2,27 +2,30 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "v3")
-	})
+	file, err := os.OpenFile("/log/k8s.log", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal("Open log file error")
+	}
+	defer file.Close()
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "pong")
+	log.SetOutput(file)
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		log.Println(req.Method, req.URL.Path, req.RemoteAddr)
+		_, _ = io.Copy(w, req.Body)
 	})
 
 	server := http.Server{Addr: ":10200"}
 	go func() {
-		log.Println("start the server")
+		log.Println("listen and serve...")
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalln("server start failed", err)
 		}
@@ -32,23 +35,6 @@ func main() {
 	signal.Notify(stopSig, syscall.SIGINT, syscall.SIGTERM)
 	<-stopSig
 
-	log.Println("graceful stopping")
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-
-	graceful := make(chan struct{})
-	go func() {
-		defer close(graceful)
-		if err := server.Shutdown(ctx); err != nil {
-			log.Println("shutdown error", err)
-		}
-	}()
-
-	select {
-	case <-ctx.Done():
-		log.Println("shutdown timeout")
-	case <-graceful:
-		log.Println("bye")
-	}
+	log.Println("graceful stopping...")
+	server.Shutdown(context.TODO())
 }
